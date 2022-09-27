9e5cb48fb0a61a4ed21ab8a54c482710e488943f
ariful4082 <ariful4082@gmail.com>
2022-09-27 21:02:40 +0600

    handle navigation

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -7,54 +7,149 @@ import Typography from "@mui/material/Typography";
 import { Link as RouterLink } from "react-router-dom";
 import useDialog from "../../hooks/useDialog";
 import PlaylistForm from "../Playlists/PlaylistForm";
-import Menus from "./Menus";
+
+import AdbIcon from "@mui/icons-material/Adb";
+import MenuIcon from "@mui/icons-material/Menu";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import * as React from "react";
+
+const pages = [
+  {
+    name: "Recent",
+    to: "/recent",
+  },
+  {
+    name: "Favorite",
+    to: "/favorite",
+  },
+  {
+    name: "Playlists",
+    to: "/",
+  },
+];
 
 const Navbar = () => {
-  const { handleOpen, handleClose, open } = useDialog();
-  const handleAddPlaylist = () => {
-    handleOpen();
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const { open, handleOpen, handleClose } = useDialog();
+
+  const handleOpenNavMenu = (event) => {
+    // setAnchorElNav(event.currentTarget);
   };
+
+  const handleCloseNavMenu = () => {
+    // setAnchorElNav(null);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="sticky" sx={{ background: "white" }}>
-        <Container maxWidth="lg">
-          <Toolbar>
-            <Typography
-              variant="h6"
-              noWrap
-              component={RouterLink}
-              to="/"
-              sx={{
-                flexGrow: 1,
-                display: {
-                  xs: "none",
-                  sm: "block",
-                  textDecoration: "none",
-                  fontSize: "40px",
-                  textShadow: "1px 1px #00dd07",
-                },
-              }}
-              color="primary"
+    <AppBar position="sticky" sx={{ background: "white" }}>
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h4"
+            noWrap
+            component={RouterLink}
+            to="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+
+              textDecoration: "none",
+            }}
+          >
+            Clean Youtube
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
             >
-              Clean Youtube
-            </Typography>
-            <Box>
-              <Menus />
-            </Box>
-            <Box>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((item, idx) => (
+                <MenuItem key={idx} onClick={handleCloseNavMenu}>
+                  <Typography
+                    to={item.to}
+                    component={RouterLink}
+                    textAlign="center"
+                  >
+                    {item.name}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h3"
+            noWrap
+            component={RouterLink}
+            to="/"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+
+              textDecoration: "none",
+            }}
+          >
+            Clean Youtube
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((item, idx) => (
               <Button
-                variant="contained"
-                color="success"
-                onClick={handleAddPlaylist}
+                key={idx}
+                to={item.to}
+                component={RouterLink}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, display: "block" }}
               >
-                Add Playlist
+                {item.name}
               </Button>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-      <PlaylistForm handleClose={handleClose} open={open} />
-    </Box>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Button variant="contained" color="success" onClick={handleOpen}>
+              Add Playlists
+            </Button>
+          </Box>
+        </Toolbar>
+        <PlaylistForm open={open} handleClose={handleClose} />
+      </Container>
+    </AppBar>
   );
 };
+
 export default Navbar;
